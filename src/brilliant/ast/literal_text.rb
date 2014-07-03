5048798735754f0277988eed2555bfbf8294ab55
Craig Buchek <craig@boochtek.com>
2014-07-02 22:39:08 -0500

    Fix the way we parse literal text.

--- src/brilliant/ast/literal_text.rb
+++ src/brilliant/ast/literal_text.rb
@@ -6,7 +6,7 @@ class Brilliant::AST::LiteralText < Brilliant::AST::Node
   attr_accessor :value
 
   def initialize(child_nodes)
-    self.value = child_nodes.first[1..-2] # Not sure why the parser isn't stripping the quotes for us; probably a bug.
+    self.value = child_nodes.first
   end
 
   def generate_code(mod, builder)
