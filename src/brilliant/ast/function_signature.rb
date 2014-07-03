b6b1f73e7557899a9a55fdda5abe57bbdbe3018c
Craig Buchek <craig@boochtek.com>
2014-07-02 22:46:47 -0500

    Check in missing VoidFunctionSignature.

--- src/brilliant/ast/function_signature.rb
+++ src/brilliant/ast/function_signature.rb
@@ -8,3 +8,10 @@ class Brilliant::AST::FunctionSignature < Brilliant::AST::Node
     self.return_type = child_nodes[-1]
   end
 end
+
+
+class Brilliant::AST::VoidFunctionSignature < Brilliant::AST::FunctionSignature
+  def initialize(child_nodes)
+    self.return_type = 'Void'
+  end
+end
